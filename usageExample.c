6a986523144853bb0adeb2b75b0bbcf5d035f5b4
syedhabibcs <urrehmsh@myumanitoba.ca>
2019-01-09 14:51:22 -0600

    added comments to usageExample

--- usageExample.c
+++ usageExample.c
@@ -6,22 +6,31 @@ int main(int argc, char *argv[])
   char *ptr;
   int i;
   Ref id1,id2,id3;
-  initPool();
   
+  // initialize pool
+  initPool();
+
+  // create object  
   id1= insertObject(100);
   printf("id1 = %lu\n", id1);
   
+  // get object
   ptr = (char*)retrieveObject(id1);
   
-  
+  // write values to object
   for (i = 0; i < 100; i++)
     ptr[i] = (char)(i%26 + 'A');
   
+  // insert another object
   id2 = insertObject(4000);
+  // insert another object
   id3 = insertObject(20200);
+  // drop reference as assignment id3 another ojbect
   dropReference(id3);
+  // insert another object
   id3 = insertObject(10);
   
+  // get object
   ptr = (char*)retrieveObject(id1);
   for (i = 0; i < 100; i++)
     fprintf(stderr,"%c",ptr[i]);
